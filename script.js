4d6e96c4eb8fd6bb7e34f7ba2bd7cc7b74eff9c5
ErikaBjornsrud <erikabjo@viken.no>
2024-02-18 22:08:12 +0100

    javascript slideshow total endringer

--- script.js
+++ script.js
@@ -1,19 +1,97 @@
 // Erika
-let slideIndex = 0;
-showSlides();
+const slideImage = document.querySelectorAll(".slide-image");
+const slidesContainer = document.querySelector(".slides-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const navigationDots = document.querySelector(".navigation-dots");
 
-function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("slide");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 20000); // Endre 20000 for å justere bildefrekvensen (20000 = 20 sekunder)
+let numberOfImages = slideImage.length;
+let slideWidth = slideImage[0].clientWidth;
+let currentSlide = 0;
+
+// Set up the slider
+
+function init() {
+  /*   
+    slideImage[0] = 0
+    slideImage[1] = 100%
+    slideImage[2] = 200%
+    */
+
+  slideImage.forEach((img, i) => {
+    img.style.left = i * 100 + "%";
+  });
+
+  slideImage[0].classList.add("active");
+
+  createNavigationDots();
 }
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+init();
+
+// Create navigation dots
+
+function createNavigationDots() {
+  for (let i = 0; i < numberOfImages; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("single-dot");
+    navigationDots.appendChild(dot);
+
+    dot.addEventListener("click", () => {
+      goToSlide(i);
+    });
+  }
+
+  navigationDots.children[0].classList.add("active");
+}
+
+// Next Button
+
+nextBtn.addEventListener("click", () => {
+  if (currentSlide >= numberOfImages - 1) {
+    goToSlide(0);
+    return;
+  }
+
+  currentSlide++;
+  goToSlide(currentSlide);
+});
+
+// Previous Button
+
+prevBtn.addEventListener("click", () => {
+  if (currentSlide <= 0) {
+    goToSlide(numberOfImages - 1);
+    return;
+  }
+
+  currentSlide--;
+  goToSlide(currentSlide);
+});
+
+// Go To Slide
+
+function goToSlide(slideNumber) {
+  slidesContainer.style.transform =
+    "translateX(-" + slideWidth * slideNumber + "px)";
+
+  currentSlide = slideNumber;
+
+  setActiveClass();
+}
+
+// Set Active Class
+
+function setActiveClass() {
+  // Set active class for Slide Image
+
+  let currentActive = document.querySelector(".slide-image.active");
+  currentActive.classList.remove("active");
+  slideImage[currentSlide].classList.add("active");
+
+  //   set active class for navigation dots
+
+  let currentDot = document.querySelector(".single-dot.active");
+  currentDot.classList.remove("active");
+  navigationDots.children[currentSlide].classList.add("active");
 }
