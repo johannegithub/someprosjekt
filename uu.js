72c92542fed0a709ac6073652b251606883b1baa
MadeleineBrath <madeleinhb@viken.no>
2024-03-04 10:06:03 +0100

    flip card

--- uu.js
+++ uu.js
@@ -3,3 +3,10 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
+
+  const card = document.getElementById("card")
+  card.addEventListener("click", flipCard);
+
+  function flipCard() {
+    card.classList.toggle("flipCard");
+  }
